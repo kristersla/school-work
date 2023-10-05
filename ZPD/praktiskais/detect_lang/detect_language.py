1ec7e20ed42de93384dd23ae79b9daeec825f9a7
Kristers Laganovskis <58227978+kristersla@users.noreply.github.com>
2023-10-05 22:58:13 +0300

    Add files via upload

--- ZPD/praktiskais/detect_lang/detect_language.py
+++ ZPD/praktiskais/detect_lang/detect_language.py
@@ -5,22 +5,25 @@ with open('praktiskais/mix_C&R/mix_C&R.json', 'r', encoding='utf-8') as f:
     comments = json.load(f)
 
 comments_not_in_english = []
-replies_not_in_english = []
+comments_in_english = []
 
 for comment in comments:
-    text = comment.get("text") 
-    if text and len(text) > 4:
+    text = comment.get("text")
+    if text and len(text) > 3:
         try:
             language = detect(text)
             if language != "en":
                 comments_not_in_english.append({"text": text, "language": language})
+            else:
+                comments_in_english.append({"text": text})
         except Exception as e:
-            print(f"An error occurred, empty 'text' list in comments.json ")
+            print(f"An error occurred, empty 'text' list in comments.json")
 
+if comments_in_english:
+    with open('praktiskais/detect_lang/only_english.json', 'w', encoding='utf-8') as f:
+        json.dump(comments_in_english, f, ensure_ascii=False, indent=4)
 
-if not comments_not_in_english:
-    print("All comments are in English")
-else:
+if comments_not_in_english:
     with open('praktiskais/detect_lang/comments_not_in_eng.json', 'w', encoding='utf-8') as f:
         json.dump(comments_not_in_english, f, ensure_ascii=False, indent=4)
 
--- ZPD/praktiskais/translation/translate.py
+++ ZPD/praktiskais/translation/translate.py
@@ -11,8 +11,12 @@ for comment in comments_data:
     translated_text = translator.translate(comment['text'], src=comment['language'], dest='en')
     translated_comments.append({'text': translated_text.text})
 
-translated_data = translated_comments
+with open('praktiskais/detect_lang/only_english.json', 'r', encoding='utf-8') as f:
+    english_comments = json.load(f)
+
+translated_comments.extend(english_comments)
+
 with open('praktiskais/translation/translated.json', 'w', encoding='utf-8') as f:
-    json.dump(translated_data, f, indent=4)
+    json.dump(translated_comments, f, indent=4)
 
 print("translated!")
