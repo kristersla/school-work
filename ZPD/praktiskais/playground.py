fe014f75c7dc89ca067d451ba3e0a8a3e83c2059
Kristers Laganovskis <58227978+kristersla@users.noreply.github.com>
2023-10-13 00:49:10 +0300

    Add files via upload

--- ZPD/praktiskais/playground.py
+++ ZPD/praktiskais/playground.py
@@ -1,44 +1,10 @@
-from langdetect import detect
-import json
+from pytube import extract
 
-with open('praktiskais/comments.json', 'r', encoding='utf-8') as f:
-    comments = json.load(f)
+#Examples
+url1='http://youtu.be/SA2iWivDJiE'
 
-comments_not_in_english = []
-replies_not_in_english = []
 
-for comment in comments:
-    text = comment.get("text") 
-    if text and len(text) > 4:
-        try:
-            language = detect(text)
-            if language != "en":
-                comments_not_in_english.append({"text": text, "language": language})
-        except Exception as e:
-            print(f"An error occurred, empty 'text' list in comments.json ")
 
-for comment in comments:
-    replies = comment.get("replies")
-    if replies:
-        for reply in replies:
-            if len(reply) > 4:
-                try:
-                    language = detect(reply)
-                    if language != "en":
-                        replies_not_in_english.append({"reply": reply, "language": language})  # Corrected key to "reply"
-                except Exception as e:
-                    print(f"An error occurred, empty 'replies' list in comments.json ")
 
-if not comments_not_in_english:
-    print("All comments are in English")
-else:
-    with open('praktiskais/comments_not_in_eng.json', 'w', encoding='utf-8') as f:
-        json.dump(comments_not_in_english, f, ensure_ascii=False, indent=4)
-
-if not replies_not_in_english:
-    print("All replies are in English")
-else:
-    with open('praktiskais/replies_not_in_eng.json', 'w', encoding='utf-8') as f:
-        json.dump(replies_not_in_english, f, ensure_ascii=False, indent=4)
-
-print("done!")
+id=extract.video_id(url1)
+print(id)
--- ZPD/praktiskais/ultimate.py
+++ ZPD/praktiskais/ultimate.py
@@ -1,9 +1,12 @@
 import googleapiclient.discovery
 import json
+from langdetect import detect
+from googletrans import Translator
 
 api_service_name = "youtube"
 api_version = "v3"
 DEVELOPER_KEY = "AIzaSyC85sT-fXJhvgfTIwJrmutBi3k_4fEbxco"
+
 
 youtube = googleapiclient.discovery.build(api_service_name, api_version, developerKey=DEVELOPER_KEY)
 
@@ -71,9 +74,8 @@ def save_comments_to_json(video_id):
     with open('praktiskais/comments.json', 'w', encoding='utf-8') as f:
         json.dump(comments, f, ensure_ascii=False, indent=4)
 
-video_id = "Hq2tv9yYDcs"
+video_id = "gwCD5971zlo"
 save_comments_to_json(video_id)
-
 
 with open('praktiskais/comments.json', 'r', encoding='utf-8') as f:
     comments = json.load(f)
@@ -89,5 +91,69 @@ for comment in comments:
     num_replies = count_replies(comment)
     total_replies += num_replies
 
-total_comments = total_text+ total_replies
+total_comments = total_text + total_replies
+
+with open('praktiskais/comments.json', 'r', encoding='utf-8') as f:
+    comments_data = json.load(f)
+
+output_data = []
+for comment in comments_data:
+    output_data.append({"text": comment["text"]})
+    for reply in comment.get("replies", []):
+        output_data.append({"text": reply})
+
+with open('praktiskais/mix_C&R/mix_C&R.json', 'w', encoding='utf-8') as file:
+    json.dump(output_data, file, indent=4, ensure_ascii=False)
+
+comments_not_in_english = []
+comments_in_english = []
+
+for comment in comments:
+    text = comment.get("text")
+    if text and len(text) > 3:
+        try:
+            language = detect(text)
+            if language != "en":
+                comments_not_in_english.append({"text": text, "language": language})
+            else:
+                comments_in_english.append({"text": text})
+        except Exception as e:
+            print(f"An error occurred, empty 'text' list in comments.json")
+
+if comments_in_english:
+    with open('praktiskais/detect_lang/only_english.json', 'w', encoding='utf-8') as f:
+        json.dump(comments_in_english, f, ensure_ascii=False, indent=4)
+
+if comments_not_in_english:
+    with open('praktiskais/detect_lang/comments_not_in_eng.json', 'w', encoding='utf-8') as f:
+        json.dump(comments_not_in_english, f, ensure_ascii=False, indent=4)
+
+translator = Translator()
+
+with open('praktiskais/detect_lang/comments_not_in_eng.json', 'r', encoding='utf-8') as f:
+    comments_data = json.load(f)
+
+translated_comments = []
+for comment in comments_data:
+    translated_text = translator.translate(comment['text'], src=comment['language'], dest='en')
+    translated_comments.append({'text': translated_text.text})
+
+with open('praktiskais/detect_lang/only_english.json', 'r', encoding='utf-8') as f:
+    english_comments = json.load(f)
+
+translated_comments.extend(english_comments)
+
+with open('praktiskais/translation/translated.json', 'w', encoding='utf-8') as f:
+    json.dump(translated_comments, f, indent=4)
+
+
 print(f"Comments: {total_comments}, Text: {total_text}, Replies: {total_replies}")
+print("translated!")
+
+
+
+
+
+
+
+
